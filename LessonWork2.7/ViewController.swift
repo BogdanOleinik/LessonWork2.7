79a9235ddc838d6ed874ea1fcb9d268818a046e0
Bogdan Oleinik <bogdan.swedbe@yandex.ru>
2022-01-19 10:44:25 +0300

    Initial Commit

---
+++ LessonWork2.7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LessonWork2.7
+//
+//  Created by Богдан Олейник on 19.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
